edc2f0ed7ff251de45420d700f670f4c81ad6f27
andre <andrevitoreur@gmail.com>
2022-09-20 16:45:03 +0100

    method GET

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const app = express();
 
 const PORT = 3333;
 
+app.get("/message", (request,response)=>{
+
+    response.send("hello world!");
+}) 
+
+/// localhost:3333/message
+
 app.listen(PORT,() => {
     console.log(`server is running on Port ${PORT}`);
 })
