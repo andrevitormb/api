86910c5afbcb68babb31f6d8f91632109b3d241b
andre <andrevitoreur@gmail.com>
2022-09-20 18:18:04 +0100

    Route Params - request.params

--- src/server.js
+++ src/server.js
@@ -4,12 +4,17 @@ const app = express();
 
 const PORT = 3333;
 
-app.get("/message", (request,response)=>{
+app.get("/message/:id/:user", (request,response)=>{
 
-    response.send("hello world!");
+    const {id, user}= request.params // Destruct object
+
+    response.send(`
+        Message ID: ${id},
+        For user: ${user},
+    `);
 }) 
 
-/// localhost:3333/message
+/// localhost:3333/message/50/Andre
 
 app.listen(PORT,() => {
     console.log(`server is running on Port ${PORT}`);
