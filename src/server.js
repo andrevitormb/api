49fae5be4c77102600bad6d2ab6a6a88e6114d04
andre <andrevitoreur@gmail.com>
2022-09-20 23:27:27 +0100

    Query Params

--- src/server.js
+++ src/server.js
@@ -9,12 +9,21 @@ app.get("/message/:id/:user", (request,response)=>{
     const {id, user}= request.params // Destruct object
 
     response.send(`
-        Message ID: ${id},
-        For user: ${user},
+        Message ID: ${id}.
+        For user: ${user}.
     `);
 }) 
 
 /// localhost:3333/message/50/Andre
+
+app.get("/user" , (request, response)=>{
+    const {page,limit} = request.query
+
+    response.send(`Page: ${page}Limit: ${limit}`)
+})
+
+///localhost:3333/user?page=1&limit=10
+/// /user (recurso) ? ( primeiro separador) & (demais separadores)
 
 app.listen(PORT,() => {
     console.log(`server is running on Port ${PORT}`);
