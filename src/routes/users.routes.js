e246fb32bcfb356bab5c101bba4f015f37852866
andre <andrevitoreur@gmail.com>
2022-09-23 14:17:54 +0100

    Middleware Learned

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,7 +6,28 @@ const usersRouter = Router();
 const UsersControllers = require("../controllers/UsersControllers");
 const usersControllers = new UsersControllers(); //instanciar na memoria para usar os methods
 
+//middleware
+
+function myMiddleware(request, response, next) {
+    console.log("you passed under middleware");
+
+    if(!request.body.isAdmin) {
+        return response.json({"message":"User unauthorized"});
+    }
+
+    next();
+
+    /* body json send 
+        {
+	"name": "Andre Vitor ",
+	"age": "28",
+	"password":"12345",
+	"isAdmin": true
+        }
+    */
+}
+
 //Path
-usersRouter.post("/",usersControllers.create);
+usersRouter.post("/",myMiddleware,usersControllers.create);
 
 module.exports = usersRouter;
