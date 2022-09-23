cb1668ebce6ecdd914d580b56382312cdcf62842
andre <andrevitoreur@gmail.com>
2022-09-23 13:59:04 +0100

    HTTP Code 201 implemented in UsersControllers

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -3,7 +3,7 @@ class UsersControllers {
     create(request,response) {
         const { name , age , password } = request.body;
     
-        response.json({name, password , age}) 
+        response.status(201).json({name, password , age}) 
     }
 }
 
